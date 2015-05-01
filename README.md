5d4240b698bc136daed36b0d56ec16357a28c531
Austin Bingham <austin.bingham@gmail.com>
2015-05-01 17:18:07 +0200

    Added a notebook.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # bokeh-playground
 Just me messing around with bokeh. Don't expect much here.
+
+## Installation
+
+Install the dependencies, start a notebook, and that's about it.
+
+```
+mkvirtualenv -p `which python3` bokeh-playground
+pip install pandas
+python setup.py install
+cd notebooks
+ipython notebook
+```
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+requires = [
+    'bokeh',
+    'ipython',
+    'jsonschema',
+    'pyzmq',
+    ]
+
+setup(name='bokeh-playground',
+      version='0.0',
+      description='Bokeh playground',
+      long_description=README,
+      license="MIT License",
+      classifiers=[
+          "Programming Language :: Python",
+      ],
+      author='Austin Bingham',
+      author_email='austin.bingham@gmail.com',
+      url='',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      )
